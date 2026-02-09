290b01908c2c46f201e271fee0d973870d6708d1
miguel-javela <migueljavela2014@gmail.com>
2026-02-09 16:33:56 -0500

    clase 7 que es el estado?

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,8 +1,15 @@
-import './TodoSearch.css'
+import React from 'react';
+import './TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     return (
-        <input placeholder="Cortar cebolla" className="TodoSearch"/>
+        <input placeholder="Cortar cebolla" className="TodoSearch" value={searchValue}
+            onChange={(event) => {
+                setSearchValue(event.target.value);
+            }}
+        />
     )
 }
 
