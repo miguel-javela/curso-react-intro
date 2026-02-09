5d2c0d8466f258758dbe447f4ff25fe3c0a0d575
miguel-javela <migueljavela2014@gmail.com>
2026-02-09 12:43:45 -0500

    clase 4 - como se comunican los componentes? props y atributos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './platzi.webp';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+const defaulTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el curso de intro a React', completed: false},
+  {text: 'Llorar con la llorona', completed: false}
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter completed={16} total={25}/>
+      <TodoSearch/>
+
+      <TodoList>
+        {defaulTodos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        ))}
+      </TodoList>
+
+      <CreateTodoButton/>
+
+    </React.Fragment>
   );
 }
 
