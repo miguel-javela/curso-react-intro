0f94f5d83f5ec19938894943e091889de9466a1f
miguel-javela <migueljavela2014@gmail.com>
2026-02-09 22:35:16 -0500

    clase 9 y 10 - buscando TODOs y completando y eliminando TODOs

--- src/App.js
+++ src/App.js
@@ -18,7 +18,33 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
-  console.log('los usuarios buscan todos de' + searchValue);
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+      //return todo.text.toLowerCase().includes(searchValue.toLowerCase());
+    }
+  );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  }
+
 
   return (
     <>
@@ -26,8 +52,12 @@ function App() {
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
 
       <TodoList>
-        {defaulTodos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} completed={todo.completed}/>
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text} text={todo.text} completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
         ))}
       </TodoList>
 
