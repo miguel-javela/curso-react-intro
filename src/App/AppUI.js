a414aa5cb81e78b0d798b658afcdf6c9c70f4e0d
miguel-javela <migueljavela2014@gmail.com>
2026-02-11 10:51:54 -0500

    clases 19 y 20
    Estados de carga y error y Actualizando estados desde useEffect

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -8,7 +8,7 @@ function AppUI ({
     completedTodos, totalTodos,
     searchValue, setSearchValue,
     searchedTodos, completeTodo,
-    deleteTodo
+    deleteTodo, loading, error
 }){
     return (
         <>
@@ -16,12 +16,16 @@ function AppUI ({
             <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue}/>
 
             <TodoList>
+                {loading && <p>Estamos cargando...</p>}
+                {error && <p>Desesperate, hubo un error...</p>}
+                {(!loading && searchedTodos.length === 0) && <p>Crea tu primer TODO</p>}
+
                 {searchedTodos.map(todo => (
-                <TodoItem
-                    key={todo.text} text={todo.text} completed={todo.completed}
-                    onComplete={() => completeTodo(todo.text)}
-                    onDelete={() => deleteTodo(todo.text)}
-                />
+                    <TodoItem
+                        key={todo.text} text={todo.text} completed={todo.completed}
+                        onComplete={() => completeTodo(todo.text)}
+                        onDelete={() => deleteTodo(todo.text)}
+                    />
                 ))}
             </TodoList>
 
--- src/App/index.js
+++ src/App/index.js
@@ -12,7 +12,7 @@ import { useLocalStorage } from './useLocalStorage';
 // localStorage.removeItem('TODOS_V1');
 
 function App() {
-  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
+  const {item: todos, saveItem: saveTodos, loading, error} = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = React.useState('');
 
   const completedTodos = todos.filter(todo => !!todo.completed).length;
@@ -54,6 +54,8 @@ function App() {
       searchedTodos={searchedTodos}
       completeTodo={completeTodo}
       deleteTodo={deleteTodo}
+      loading={loading}
+      error={error}
     />
   );
 }
