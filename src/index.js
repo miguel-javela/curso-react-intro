d6e830b7f9465e7c85f41fa8fbea0c6ea0d79411
migueljav13 <102913589+miguel-javela@users.noreply.github.com>
2026-02-17 11:54:01 -0500

    clase 13 - curso de Reactjs: Patrones de render y composicion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
+
+function App(props){
+    return (
+        <h1>¡{props.saludo}, {props.nombre}!</h1>
+    )
+}
+
+function withSaludo (WrappedComponent) {
+    return function WrappedComponentWithSaludo(saludo){
+
+        return function ComponenteDeVerdad (props) {
+            return (
+                <>
+                    <WrappedComponent {...props} saludo={saludo}/>
+                    <p>estamis acompañando al WrappedComponent</p>
+                </>
+            )
+        }
+    }
+
+
+}
+
+const AppWithSaludo = withSaludo(App)('wenitas');
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    // < App saludo="Buenas" nombre="Nath" />
+    < AppWithSaludo nombre="Nath" />
+);
