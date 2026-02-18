10504d70669e92a37ac1f45621106c6802cb6663
migueljav13 <102913589+miguel-javela@users.noreply.github.com>
2026-02-17 22:37:47 -0500

    clase 16 - curso de React.js: patrones de render y composicion

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import './ChangeAlert.css'
 
 function ChangeAlert({show, toggleShow}){
     if(show){
         return (
-            <div>
-                <p>Hubo cambios</p>
-                <button 
-                    onClick={() => toggleShow(false)}
-                >
-                    volver a cargar la informacion
-                </button>
+            <div className="ChangeAlert-bg">
+                <div className="ChangeAlert-container">
+                    <p>Parece que cambiaste tus TODOs en otra pestaña o ventana del navegados</p>
+                    <p>¿Quieres sincronizar tus TODOs?</p>
+                
+                    <button
+                        className="TodoFormo-button TodoForm-button-add"
+                        onClick={toggleShow}
+                    >
+                        Yes!
+                    </button>
+                </div>
             </div>
-        );        
+        );
     } else {
         return null;
     }
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -12,10 +12,7 @@ function TodoList(props){
 
             {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchText)}
 
-            {props.searchedTodos.map(renderFunc)}
-            <ul className="TodoList">
-                {/* {children} */}
-            </ul>
+            {(!props.loading && !props.error) && props.searchedTodos.map(renderFunc)}
         </section>
 
     );
